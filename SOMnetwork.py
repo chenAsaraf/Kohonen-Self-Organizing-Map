06ce5fa36a737c2d4552a0464ae4038c5a2bc393
chenAsaraf <44566578+chenAsaraf@users.noreply.github.com>
2020-01-13 15:13:55 +0200

    Update SOMnetwork.py

--- SOMnetwork.py
+++ SOMnetwork.py
@@ -5,13 +5,19 @@ pattern_length = 10
 
 # Time: increase with every new data point
 time = 0
-number_iteration = 1000
+number_iteration = 10000
 # After some time learning rate will converge to zero
-max_time_learning_rate_update = 150
-time_constant = int(0.05 * number_iteration)
+max_time_learning_rate_update = 1500
+time_constant = int(0.1 * number_iteration) # = TAU IN LECTURE
 #
-alpha_0 = 1.0
+alpha_0 = 1.0 # = ETA IN LECTURE
 alpha_infinte = 0.05
+# initial radius: % of
+init_rad = 50 # = SIGMA IN LECTURE
+
+
+
+
 
 
 
@@ -34,7 +40,7 @@ def update_location(max_activation_unit):
 # This learning rate indicates how much we want to adjust our weights.
 # After time max_time_learning_rate_update (positive infinite), this learning rate will converge to zero
 # so we will have no update even for the neuron winner .
-def learning_rate:
+def learning_rate():
     return alpha_0 * np.exp(-float(t) / tau)
 
 
